3325a1e553e83442c7311d6d4f4ed919924268eb
LuoTianOrange <2977277015@qq.com>
2026-03-20 02:54:06 +0800

    ✨ feat: 修改PlantTrees卡牌类型为能力牌

--- Cards/Uncommon/PlantTrees.cs
+++ Cards/Uncommon/PlantTrees.cs
@@ -4,7 +4,7 @@ using STS2RitsuLib.Scaffolding.Content;
 namespace STS2_WineFox.Cards.UnCommon
 
 {
-        public class PlantTrees() : WineFoxCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+        public class PlantTrees() : WineFoxCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
         {
         public override CardAssetProfile AssetProfile => new(
             Const.Paths.CardPlantTrees,
