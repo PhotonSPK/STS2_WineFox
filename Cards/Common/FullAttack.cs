439dcbe25ad4f7f8962f69e830ce674dee046463
LuoTianOrange <2977277015@qq.com>
2026-03-19 16:54:18 +0800

    ✨ feat: 添加了全力一击卡面的动态变量

--- Cards/Common/FullAttack.cs
+++ Cards/Common/FullAttack.cs
@@ -1,7 +1,9 @@
 ﻿using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using STS2_WineFox.Powers;
 using STS2RitsuLib.Scaffolding.Content;
@@ -10,11 +12,33 @@ namespace STS2_WineFox.Cards.Common
 {
     public class FullAttack() : WineFoxCard(2, CardType.Attack, CardRarity.Common, TargetType.None)
     {
+        private sealed class TotalHitsVar() : DynamicVar("Hits", 0m)
+        {
+            public override void UpdateCardPreview(
+                CardModel card, CardPreviewMode previewMode,
+                Creature? target, bool runGlobalHooks)
+            {
+                PreviewValue = CalcHits(card);
+            }
+
+            protected override decimal GetBaseValueForIConvertible()
+                => CalcHits(_owner as CardModel);
+
+            private static decimal CalcHits(CardModel? card)
+            {
+                var creature = card?._owner?.Creature;
+                if (creature == null) return 0m;
+                var wood = creature.Powers.OfType<WoodPower>().FirstOrDefault()?.Amount ?? 0;
+                var stone = creature.Powers.OfType<StonePower>().FirstOrDefault()?.Amount ?? 0;
+                return wood + stone;
+            }
+        }
+        
         protected override IEnumerable<string> RegisteredKeywordIds =>
             [WineFoxKeywords.Wood, WineFoxKeywords.Stone];
 
         protected override IEnumerable<DynamicVar> CanonicalVars =>
-            [new DamageVar(4m, ValueProp.Move)];
+            [new DamageVar(4m, ValueProp.Move), new TotalHitsVar()];
 
         public override CardAssetProfile AssetProfile => new(
             Const.Paths.CardFullAttack,
@@ -44,9 +68,10 @@ namespace STS2_WineFox.Cards.Common
             var stoneAmount = stonePower?.Amount ?? 0;
             var totalHits = woodAmount + stoneAmount;
 
+
             if (totalHits <= 0) return;
 
-            // 消耗所有资源（applier 传 null，跳过 HandCrank 倍增钩子）
+            // 消耗所有资源
             if (woodPower != null && woodAmount > 0)
                 await PowerCmd.ModifyAmount(woodPower, -(decimal)woodAmount, null, this);
             if (stonePower != null && stoneAmount > 0)
